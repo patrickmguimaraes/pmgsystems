89b5f18705c46575ede007e0769c1dd255213039
Patrick Guimarães <patrickguimaraes@PMG-MacBook-Pro.local>
2022-04-21 14:39:48 -0300

    Primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  title = 'PMG Systems';
+  public dateNow = new Date();
+  public dDay = new Date('May 17 2022 00:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference: number;
+  public secondsToDday: number;
+  public minutesToDday: number;
+  public hoursToDday: number;
+  public daysToDday: number;
+
+  constructor() { }
+
+  private getTimeDifference () {
+      this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+      this.allocateTimeUnits(this.timeDifference);
+  }
+
+private allocateTimeUnits (timeDifference: any) {
+      this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+      this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+      this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+      this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+}
+
+  ngOnInit() {
+     this.subscription = interval(1000)
+         .subscribe(x => { this.getTimeDifference(); });
+  }
+
+ ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
+}
